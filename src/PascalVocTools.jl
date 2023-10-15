7bde12b44af0c046190c39ad1d8e0c534053af7a
cirobr <ciro.rosa@alumni.usp.br>
2023-10-15 10:47:38 +0000

    initial commit

--- src/PascalVocTools.jl
+++ src/PascalVocTools.jl
@@ -1,7 +1,11 @@
 module PascalVocTools
 
 export pascalvoc_classnumbers, pascalvoc_classnames, pascalvoc_colormap, pascalvoc_rgb2class
+
+import ImageCore
+import Colors: RGB, Gray, N0f8
 import FLoops: @floop
+
 
 include("./pascalvoc.jl")
 
