d1285a838e455348075c11711f18faf9224cae74
cirobr <ciro.rosa@alumni.usp.br>
2023-10-15 20:00:23 +0000

    added dependency

--- src/PascalVocTools.jl
+++ src/PascalVocTools.jl
@@ -3,6 +3,7 @@ module PascalVocTools
 
 export pv_classnumbers, pv_classnames, pv_colormap, pv_rgb2class
 
+import DataStructures
 import ImageCore
 import Colors: RGB, Gray, N0f8
 import FLoops: @floop
