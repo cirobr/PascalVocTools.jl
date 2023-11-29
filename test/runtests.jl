725e172e89f73b3f76bdcbab631e094d28e69502
cirobr <ciro.rosa@alumni.usp.br>
2023-11-29 08:24:59 -0300

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using PascalVocTools
 using Test
 
 @testset "PascalVocTools.jl" begin
-    # Write your tests here.
+    @test voc_classnumber2classname[10] == "cow"
+    @test voc_classname2classnumber["cow"] == 10
+    @test voc_colormap2classnumber[[64, 128, 0]] == 10
+    @test voc_classnumber2colormap[10] == [64, 128, 0]
 end
