7c52a7b68fb5c89ee27a7db9acb6f69bf7aff494
cirobr <ciro.rosa@alumni.usp.br>
2026-09-13 14:52:24 -0300

    simplified testing

--- test/conversions_tests.jl
+++
@@ -1,5 +0,0 @@
-@test pvt.classnumber2classname(10) == "cow"
-@test pvt.classnumber2classname(30) == "void"  # 30 is not a valid class
-
-@test pvt.classname2classnumber("cow") == 10
-@test pvt.classname2classnumber("unknown") == 255  # "unknown" is not a valid class
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using PascalVocTools; pvt=PascalVocTools
 using Test
 
 @testset "PascalVocTools.jl" begin
-    include("conversions_tests.jl")
+    @test pvt.classnumber2classname(0) == "background"
+    @test pvt.classname2classnumber("background") == 0
+    @test pvt.classnumber2classname(255) == "void"
+    @test pvt.classname2classnumber("void") == 255
+    @test pvt.classnumber2classname(10) == "cow"
+    @test pvt.classname2classnumber("cow") == 10
 end
